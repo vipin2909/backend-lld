213c6b770f6308fc4d822adab463a9ecf77f9855
chiku <vipinsharma2909@gmail.com>
2024-01-25 22:28:59 +0530

    Implemented Student class with attributes and Methods

--- backend-lld1/class-1/src/Main.java
+++ backend-lld1/class-1/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student vipin = new Student();
+        vipin.name = "Vipin";
+        vipin.age = 24;
+        vipin.gender = "Male";
+
+        Student ritika = new Student();
+        ritika.name = "Ritika";
+        ritika.age = 24;
+        ritika.gender = "Female";
+
+        System.out.println(vipin);
+
     }
 }
--- backend-lld1/class-1/src/Student.java
+++ backend-lld1/class-1/src/Student.java
@@ -6,10 +6,24 @@
 // Example of a student entity
 
 public class Student {
+
+//    public String toString() {
+//        return "Student Details:\n\n"+
+//                "Name: "+name+"\n"+
+//                "Age: "+age+"\n"+
+//                "Gender: "+gender+"\n";
+//    }
     String name;
     int age;
     String gender;
 
+    void changeBatchName() {
+        System.out.println("Change Batch");
+    }
+
+    void giveMockInterview() {
+        System.out.println("Giving Mock Interview....");
+    }
     void print() {
         System.out.println("Student Name: " + name);
     }
