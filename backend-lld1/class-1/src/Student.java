247bbeca126636f28037c893d31558e366e8b600
chiku <vipinsharma2909@gmail.com>
2024-01-25 21:30:53 +0530

    Backend LLD Module 1

---
+++ backend-lld1/class-1/src/Student.java
@@ -0,0 +1,16 @@
+// OOP
+
+// 1. Software should revolve around entities
+// 2. Each entity has its own attributes (info) & also has its own behaviours.
+
+// Example of a student entity
+
+public class Student {
+    String name;
+    int age;
+    String gender;
+
+    void print() {
+        System.out.println("Student Name: " + name);
+    }
+}
