c39565fcee56a797a5f90b26b845b0c1891b1811
chiku <vipinsharma2909@gmail.com>
2024-02-02 00:37:02 +0530

    Implemented code for threads and processes class & class notes

---
+++ backend-lld1/IntroToProcessAndThreads/src/threads/Client.java
@@ -0,0 +1,7 @@
+package threads;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Current Thread : " + Thread.currentThread().getName());
+    }
+}
