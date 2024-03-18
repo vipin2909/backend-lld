d7ec71aed286a3869a94dcb3beddfcf25a7b3d59
chiku <vipinsharma2909@gmail.com>
2024-03-18 22:58:21 +0530

    Implemented Basic Singleton Code which using static instance and private constructor

---
+++ backend-lld2/Singleton/src/basicSingleton/BasicSingleton.java
@@ -0,0 +1,23 @@
+package basicSingleton;
+
+public class BasicSingleton {
+
+
+    private BasicSingleton() {
+    }
+    private static final BasicSingleton INSTANCE = new BasicSingleton();
+
+    public static BasicSingleton getInstance() {
+        return INSTANCE;
+    }
+
+    private int value = 0;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
--- backend-lld2/Singleton/src/basicSingleton/Demo.java
+++ backend-lld2/Singleton/src/basicSingleton/Demo.java
@@ -1,2 +1,9 @@
-package basicSingleton;public class Demo {
+package basicSingleton;
+
+public class Demo {
+    public static void main(String[] args) {
+        BasicSingleton singleton = BasicSingleton.getInstance();
+        singleton.setValue(123);
+        System.out.println(singleton.getValue());
+    }
 }
